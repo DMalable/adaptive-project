3c847ad77b5d1f66b3a5a9eeee5c919b7b879b23
Dmitriy Malakhov <malakhov_88@mail.ru>
2022-02-03 01:39:08 +0300

    Добавлена секция команда

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
-const fullscreenMenu = document.querySelector('.fullscreen-menu')
-const hamburger = document.querySelector('.hamburger')
-const closeSign = document.querySelector('.fullscreen-menu__close')
-const menuLink = document.querySelectorAll('.menu__link')
+const fullscreenMenu = document.querySelector(".fullscreen-menu");
+const hamburger = document.querySelector(".hamburger");
+const closeSign = document.querySelector(".fullscreen-menu__close");
+const menuLink = document.querySelectorAll(".menu__link");
 
-hamburger.addEventListener('click', function(event){
+hamburger.addEventListener("click", function (event) {
   event.preventDefault();
-  fullscreenMenu.classList.add('fullscreen-menu--active')
-})
+  fullscreenMenu.classList.add("fullscreen-menu--active");
+});
 
-closeSign.addEventListener('click', function(event){
-  event.preventDefault()
-  fullscreenMenu.classList.remove('fullscreen-menu--active')
-})
+closeSign.addEventListener("click", function (event) {
+  event.preventDefault();
+  fullscreenMenu.classList.remove("fullscreen-menu--active");
+});
 
-menuLink.forEach(elm => elm.addEventListener('click', () => fullscreenMenu.classList.remove('fullscreen-menu--active')
-))
+menuLink.forEach((elm) => elm.addEventListener("click", () => fullscreenMenu.classList.remove("fullscreen-menu--active")));
+
+// часть на jquerry
+
+$(window).on("load", () => {
+  $(document).ready(function () {
+    $(".single-item").slick();
+  });
+
+  const employeeName = $(".employee__name");
+  $(employeeName).on("click", (e) => {
+    $(e.target).toggleClass("employee__name--active");
+    employeeName.not($(e.target)).each((ndx, item) => {
+      $(item).removeClass("employee__name--active");
+    });
+  });
+});
